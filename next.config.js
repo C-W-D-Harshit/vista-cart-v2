9d21b571b534f7e336130399b85cd51b918a056a
Harshit Sharma <cwd.harshit911@gmail.com>
2023-09-23 18:01:55 +0530

    prod

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "res.cloudinary.com"],
   },
+  experimental: {
+    esmExternals: false,
+  },
 };
 
 module.exports = nextConfig;
